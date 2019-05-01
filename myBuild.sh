52535873cf64082d31493eb83d34900fdf0cb15b
thomas <258333601@163.com>
2019-05-02 06:36:00 +0800

    new

---
+++ myBuild.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+scriptPath=$(cd `dirname $0`; pwd)
+#工程根目录 需要替换成自己本机的工程根目录
+workRoot=${scriptPath}
+#ios工程目录
+iosRoot=${workRoot}/ios
+#scriptPath=$(cd `dirname $0`; pwd)
+androidRoot=${workRoot}/android
+#工程名
+targetName="rdMyAPP"
+#时间
+buildTime=`date "+%Y%m%d"`
+ipaName=rd.ipa
+
+#产品输出目录
+#outPutDir=${scriptPath}/out
+
+
+# CODE_SIGN_IDENTITY="iPhone Distribution: ALBARINA"
+
+
+   cd ${iosRoot}
+
+   buildPath=build/build
+   payloadPath=build/temp/Payload
+   appFileFullPath=${buildPath}/Build/Products/Release-iphoneos/${targetName}.app
+
+   mkdir -p ${buildPath} ${payloadPath} ${buildPath}
+   xcodebuild  -scheme ${targetName}  -sdk iphoneos -derivedDataPath  ${buildPath}  -project ${iosRoot}/rdMyAPP.xcodeproj  -configuration Release  CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO
+   if [ $? -eq 0 ];then
+      echo ${app} '编译成功'
+   else
+     echo '编译失败'  
+     exit -1;
+   fi
+   cp -r ${appFileFullPath} ${payloadPath}
+    # 打包并生成 .ipa 文件
+    cd build/temp
+    zip -q -r ${ipaName} Payload
+
+
+  if [ $? -eq 0 ];then	
+      echo '打包签名成功'
+  else
+      echo "打包失败 签名错误" 
+      exit -1;
+  fi
+
+echo "ipa包在 ${build} 目录下,上传 deploy成功！";
+done
