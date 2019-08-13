8e155c61f52270a9353e2712bf42ce19e766e1d3
thomas <258333601@163.com>
2019-08-13 21:18:27 +0800

    Merge branches 'rdApp' and 'zrd' of https://github.com/tavenzhang/rn into rdApp
    # Conflicts:
    #	ios/rdMyAPP.xcodeproj/project.pbxproj
    #	ios/rdMyAPP/Images.xcassets/LaunchImage.launchimage/Contents.json

--- shell/start.sh
+++ shell/start.sh
@@ -2,4 +2,4 @@
 
 #sh reSign.sh 'source.ipa' 'Beehive Studios Inc' './sign/365_Beehive.mobileprovision'  'target.ipa'
 #sh reSign.sh 'source.ipa' 'Techno Construction LLC' './sign/01_LLC.mobileprovision'  'target.ipa'
-sh reSign.sh 'source.ipa' 'Beijing Yinlibo Technology Co. Ltd.' './sign/yjb.mobileprovision'  'target.ipa'
+#sh reSign.sh 'source.ipa' 'Beijing Yinlibo Technology Co. Ltd.' './sign/yjb.mobileprovision'  'target.ipa'
