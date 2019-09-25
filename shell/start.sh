1fd9df8f5ef3c075c2eb76aebca773ca56e215da
taven <abcd1234>
2019-09-25 19:56:53 +0800

    new

--- shell/start.sh
+++ shell/start.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 #sh reSign.sh 'source.ipa' 'Beehive Studios Inc' './sign/365_Beehive.mobileprovision'  'target.ipa'
-sh reSign.sh 'source.ipa' 'Techno Construction LLC' './sign/01_LLC.mobileprovision'  'target.ipa'
+#sh reSign.sh 'source.ipa' 'Techno Construction LLC' './sign/01_LLC.mobileprovision'  'target.ipa'
 #sh reSign.sh 'source.ipa' 'Beijing Yinlibo Technology Co. Ltd.' './sign/yjb.mobileprovision'  'target.ipa'
+sh reSign.sh 'source.ipa' 'Beehive Studios Inc' './sign/365.mobileprovision'  'target.ipa'
